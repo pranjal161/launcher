130ec686053bd332fccd5c7e41d8c3035629ea05
spamecha2 <suhani.pamecha@dxc.com>
2021-03-11 11:17:02 +0530

    Code refactoring

--- src/components/financialTable/financialTable.tsx
+++ src/components/financialTable/financialTable.tsx
@@ -3,13 +3,24 @@ import { useTranslation } from 'react-i18next';
 import { DxcTable } from '@dxc-technology/halstack-react';
 import axios from 'axios';
 import { AppConfig } from '../../config/appConfig';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Table from "../../components/table/table";
 import React from 'react';
 
 const FinancialTable = (props: { clientUrl: any }) => {
     const { t } = useTranslation();
     const config = AppConfig;
-    const [financialData, setFinancialData] = React.useState([]);
+    const [premiumUrl, setPremiumUrl] = useState('');
+    const financialDataColumns = [
+        { label: '_OPERATION', property: 'premium:type' },
+        { label: '_CONTRACT_IDENTIFIER', property: 'contract:number' },
+        { label: '_EFFECTIVE_DATE', property: 'operation:start_date',type: "date" },
+        { label: '_STATUS_DATE', property: 'operation:status_date', type: "date" },
+        { label: '_STATUS', property: 'operation:status_label' },
+        { label: '_GROSS_AMOUNT', property: 'operation:amount' },
+        { label: '_NET_AMOUNT', property: 'operation:net_amount' },
+    ]
+
 
     useEffect(() => {
         getData();
@@ -20,44 +31,17 @@ const FinancialTable = (props: { clientUrl: any }) => {
             axios.get(props.clientUrl, { headers: config.headers }).then(res => {
                 const clientNumber = res.data['person:client_number'];
                 const premiumUrl = AppConfig.hostUrl.defaultHostUrl + 'financials/premiums?person:client_number=' + clientNumber + '&organization:client_number=';
-                axios.get(premiumUrl, { headers: config.headers }).then(res => {
-                    if (res && res.data && res.data._links && res.data._links.item) {
-                        res.data._links.item = Array.isArray(res.data._links.item) ? res.data._links.item : [res.data._links.item]
-                        res.data._links.item.forEach((element: { summary: { [x: string]: string; }; href: string }) => {
-                            setFinancialData(res.data._links.item);
-                        });
-                    }
-                });
-            })
+                setPremiumUrl(premiumUrl);
+            });
         }
+
     }
     return (
         <>
-            {financialData.length > 0 && (
-                <DxcTable>
-                    <tr>
-                        <th>{t('_OPERATION')}</th>
-                        <th>{t('_CONTRACT_IDENTIFIER')}</th>
-                        <th>{t('_EFFECTIVE_DATE')}</th>
-                        <th>{t('_STATUS_DATE')}</th>
-                        <th>{t('_STATUS')}</th>
-                        <th>{t('_GROSS_AMOUNT')}</th>
-                        <th>{t('_NET_AMOUNT')}</th>
-                    </tr>
-                    {financialData.map((row) => (
-                        <tr key={row['href']}>
-                            <td>{row['summary']['premium:type']}</td>
-                            <td>{row['summary']['contract:number']}</td>
-                            <td>{row['summary']['operation:start_date']}</td>
-                            <td>{row['summary']['operation:status_date']}</td>
-                            <td>{row['summary']['operation:status_label']}</td>
-                            <td>{row['summary']['operation:amount']}</td>
-                            <td>{row['summary']['operation:net_amount']}</td>
-                        </tr>
-                    ))}
-                </DxcTable>
+            {premiumUrl && (
+                < Table url={premiumUrl} columnId={financialDataColumns} />
             )}
         </>
-    );
+    )
 }
 export default FinancialTable
