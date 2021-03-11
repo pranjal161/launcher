aca48ea36f2d5606b551a2c11ead2876ae7b94a9
jlalan <jlalan@dxc.com>
2021-03-11 20:28:34 +0530

    fix for organization

--- src/components/financialTable/financialTable.tsx
+++ src/components/financialTable/financialTable.tsx
@@ -1,6 +1,4 @@
-import { makeStyles } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
-import { DxcTable } from '@dxc-technology/halstack-react';
 import axios from 'axios';
 import { AppConfig } from '../../config/appConfig';
 import { useEffect, useState } from 'react';
@@ -11,6 +9,8 @@ const FinancialTable = (props: { clientUrl: any }) => {
     const { t } = useTranslation();
     const config = AppConfig;
     const [premiumUrl, setPremiumUrl] = useState('');
+    let client_number;
+    let premium;
     const financialDataColumns = [
         { label: '_OPERATION', property: 'premium:type' },
         { label: '_CONTRACT_IDENTIFIER', property: 'contract:number' },
@@ -28,13 +28,21 @@ const FinancialTable = (props: { clientUrl: any }) => {
     const getData = () => {
         if (props.clientUrl) {
             axios.get(props.clientUrl, { headers: config.headers }).then(res => {
-                const clientNumber = res.data['person:client_number'];
-                const premiumUrl = AppConfig.hostUrl.defaultHostUrl + 'financials/premiums?person:client_number=' + clientNumber + '&organization:client_number=';
-                setPremiumUrl(premiumUrl);
+                if (props.clientUrl.includes('persons')) {
+                    client_number = res.data['person:client_number'];
+                    premium = AppConfig.hostUrl.defaultHostUrl + 'financials/premiums?person:client_number=' + client_number;
+                    setPremiumUrl(premium);
+                } else if (props.clientUrl.includes('organizations')) {
+                    client_number = res.data['organization:client_number'];
+                    premium = AppConfig.hostUrl.defaultHostUrl + 'financials/premiums?organization:client_number=' + client_number;
+                    setPremiumUrl(premium);
+                }
+
             });
         }
-
     }
+
+
     return (
         <>
             {premiumUrl && (
