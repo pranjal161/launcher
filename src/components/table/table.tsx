71cc7d7e6e11f90613c73ed094d8fbe126e66dba
jlalan <jlalan@dxc.com>
2021-03-12 14:32:27 +0530

    paginator

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -5,11 +5,13 @@ import { useTranslation } from "react-i18next";
 import { ApplicationContext } from "../../context/applicationContext";
 import { StyledHoverRow } from '../../styles/global-style';
 import { getDescriptionValue } from "../../util/functions";
+import Paginator from '../paginator/paginator';
 
 const Table = (props: { url: string; columnId: any[] }) => {
     const applicationContext = useContext(ApplicationContext);
-    const [tableData, setTableData] = useState<undefined|any>();
+    const [tableData, setTableData] = useState<undefined | any>();
     const { t } = useTranslation();
+    const [totalItems, changeTotalItems] = useState(0);
 
     useEffect(() => {
         getData();
@@ -22,38 +24,42 @@ const Table = (props: { url: string; columnId: any[] }) => {
                 if (!Array.isArray(response.data['_links']['item'])) {
                     response.data['_links']['item'] = [response.data['_links']['item']];
                 }
-                setTableData(response.data)
+                const count = response && response.data && response.data._count;
+                setTableData(response.data);
+                changeTotalItems(count === '500+' ? 500 : count);
             }
         });
     }
 
     return (
         <>
-            {tableData && tableData._links && tableData._links.item && tableData._links.item.length > 0 ? (<DxcTable>
-                <tr>
-                    {props.columnId.map(columnItem => (
-                        <th key={columnItem['label']}>
-                            {t(columnItem['label'])}
-                        </th>
-                    ))}
-                </tr>
-
-                {tableData._links.item.map((row: any) => (
-                    <StyledHoverRow key={row['href']}>
-                        {  props.columnId.map(columnItem => (
-                            <td>
-                                { typeof (columnItem.property) === "object" ? (
-                                    columnItem.property.map((id: string) => (
-                                        row['summary'][id]
-                                    ))
-                                    // property is an array then concatenate
-                                ) : (getDescriptionValue(row['summary'][columnItem.property], columnItem.property, tableData, columnItem.type))
-                                }
-                            </td>
+            {tableData && tableData._links && tableData._links.item && tableData._links.item.length > 0 ? (<>
+                <DxcTable>
+                    <tr>
+                        {props.columnId.map(columnItem => (
+                            <th key={columnItem['label']}>
+                                {t(columnItem['label'])}
+                            </th>
                         ))}
-                    </StyledHoverRow>
-                ))}
-            </DxcTable>) : (<DxcTable >
+                    </tr>
+                    {tableData._links.item.map((row: any) => (
+                        <StyledHoverRow key={row['href']}>
+                            {  props.columnId.map(columnItem => (
+                                <td>
+                                    { typeof (columnItem.property) === "object" ? (
+                                        columnItem.property.map((id: string) => (
+                                            row['summary'][id]
+                                        ))
+                                        // property is an array then concatenate
+                                    ) : (getDescriptionValue(row['summary'][columnItem.property], columnItem.property, tableData, columnItem.type))
+                                    }
+                                </td>
+                            ))}
+                        </StyledHoverRow>
+                    ))}
+                </DxcTable>
+                <Paginator totalItems={totalItems} itemsPerPage={5} data={tableData} handler={getData} />
+            </>) : (<DxcTable >
                 <tr>
                     {props.columnId.map(columnItem => (
                         <th>
