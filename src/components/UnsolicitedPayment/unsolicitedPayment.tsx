0c38b762451392a3fa7d190f82ad2ffb159f7125
akushwaha21 <akruti.kushwaha@dxc.com>
2021-03-15 16:39:39 +0530

    Alert Component

--- src/components/UnsolicitedPayment/unsolicitedPayment.tsx
+++ src/components/UnsolicitedPayment/unsolicitedPayment.tsx
@@ -3,6 +3,8 @@ import React, { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { ApplicationContext } from "../../context/applicationContext";
 import { DxcTable, DxcButton, DxcInput } from "@dxc-technology/halstack-react";
+import { getStatusReport } from "../../util/functions";
+import Alert from '../../components/alert/alert';
 
 const UnsolicitedPayment = (props: { response: any; onClickDialog: () => void; }) => {
 	const url = props.response._links.self.href;
@@ -12,6 +14,7 @@ const UnsolicitedPayment = (props: { response: any; onClickDialog: () => void; }
 	const [total, setTotal] = useState(0);
 	const [investmentSplitPayload, setInvestmentSplitPayload] = useState<any>([])
 	const { t } = useTranslation();
+	const [list, setList] = useState([]);
 
 	useEffect(() => {
 
@@ -84,6 +87,8 @@ const UnsolicitedPayment = (props: { response: any; onClickDialog: () => void; }
 			'investment_split': investmentSplitPayload
 		}
 		axios.patch(url, payload, { headers: applicationContext.headers }).then((res) => {
+			const status_report = getStatusReport(res);
+			setList(status_report);
 			if (res && res.data._embedded['cscaia:status_report'] && res.data._embedded['cscaia:status_report'].consistent) {
 				if (res.data._embedded['cscaia:execute']) {
 					const transferUrl = url + '/execute';
@@ -108,6 +113,7 @@ const UnsolicitedPayment = (props: { response: any; onClickDialog: () => void; }
 		<>
 			{funddata.length > 0 && (
 				<>
+					<Alert toastList={list} />
 					<div className="col-12 pb-4">
 						<DxcInput
 							label={t('_GROSS_AMOUNT')}
---
+++ src/components/alert/alert.tsx
@@ -0,0 +1,39 @@
+import { DxcAlert } from "@dxc-technology/halstack-react";
+import React, { useState, useEffect } from 'react';
+import { StyledMessageContainer } from '../../styles/global-style';
+
+const Alert = (props: { toastList: any }) => {
+    const { toastList } = props;
+    const [list, setList] = useState(toastList);
+    console.log('toastList -->', toastList);
+
+    useEffect(() => {
+        setList(toastList);
+    }, [toastList]);
+
+    useEffect(() => {}, [toastList, list]);
+
+    return (
+        <>
+          {list && !list.consistent && list['messages'] && list['messages'].length > 0 && 
+            <StyledMessageContainer onClick={() => setList({})}>
+              <div>
+                {
+                    list['messages'].map((toast: any, i: number) =>     
+                      <DxcAlert
+                        key={i}
+                        type={ toast.severity === 'warn' ? 'warning' : toast.severity }
+                        mode="inline"
+                        inlineText={toast.message}
+                        margin="xxsmall"
+                      />
+                    )
+                }
+              </div>
+            </StyledMessageContainer>
+          }
+        </>
+    );
+}
+
+export default Alert;
