8b91dd12acb5f54f10e109435d8a4a7ac36f0fc6
akushwaha21 <akruti.kushwaha@dxc.com>
2021-03-24 18:03:39 +0530

    Code cleanup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import {Route, Switch } from "react-router-dom";
-import { RouteProps } from "react-router";
 import routes from "./routes";
 
 export interface CustomProps {
