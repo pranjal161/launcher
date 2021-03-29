bf26c6456fd0e47f1ecd363f8410f28f08ce7b1f
=Norbert POINTU <=npointu@dxc.com>
2021-03-29 10:02:44 +0200

    test CommunicateAcrossBrowser

--- src/views/training/TrainingNorbert/TrainingNorbert.js
+++ src/views/training/TrainingNorbert/TrainingNorbert.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {DxcTabs} from "@dxc-technology/halstack-react"
 import TicketListPage from "./components/TicketListPage/TicketListPage";
 import LatestPage from "./components/LatestPage/LatestPage";
+import CommunicateAcrossBrowser from "./components/CommunicateAcrossBrowser/CommunicateAcrossBrowser";
 
 
 function TrainingNorbert(props) {
@@ -20,6 +21,9 @@ function TrainingNorbert(props) {
                         label: "Latest"
                     },
                     {
+                        label: "Communicate across browser"
+                    },
+                    {
                         label: "Ticket list"
                     }
                 ]}
@@ -28,8 +32,11 @@ function TrainingNorbert(props) {
             {activeTab === 0 && (
                 <LatestPage/>
             )}
-
             {activeTab === 1 && (
+                <CommunicateAcrossBrowser/>
+            )}
+
+            {activeTab === 2 && (
                 <TicketListPage/>
             )}
         </>
