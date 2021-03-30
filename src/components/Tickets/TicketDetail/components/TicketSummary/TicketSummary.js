00c92b69662e3fa781825ba88f2f48d38ae724fd
spamecha2 <suhani.pamecha@dxc.com>
2021-03-30 17:43:36 +0530

    DAPDIG-107

--- src/components/Tickets/TicketDetail/components/TicketSummary/TicketSummary.js
+++ src/components/Tickets/TicketDetail/components/TicketSummary/TicketSummary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, CardContent} from "@material-ui/core";
+import { Card, CardContent } from "@material-ui/core";
 import DataLine from "./components/DataLine/DataLine";
 import Label from "./components/Label/Label";
 import Section from "./components/Section/Section";
@@ -7,29 +7,57 @@ import LinkedContract from "./components/LinkedContract/LinkedContract";
 import LinkedClient from "./components/LinkedClient/LinkedClient";
 import Sections from "./components/Sections/Sections";
 import Upload from "../Upload/Upload";
-import {formatValue} from "../../../../../util/functions";
+import { formatValue } from "../../../../../util/functions";
 import useDeskUsers from "../../../../../data/hooks/useDeskUsers";
-import {DxcChip} from '@dxc-technology/halstack-react';
+import { DxcChip } from '@dxc-technology/halstack-react';
 import moment from "moment";
 import useDeskTickets from "../../../../../data/hooks/useDeskTickets";
+import { AddIcon, DotsIcon } from "../../../../../../src/assets/svg";
+import { StyledButton } from '../../../../../../src/styles/global-style';
 
-const Divider = () => <hr className="solid"/>
+const Divider = () => <hr className="solid" />
 
 
-const TicketSummary = ({ticket}) => {
+const TicketSummary = ({ ticket }) => {
     const TitleValue = () => {
         return (<>{ticket.title}</>)
     }
-    const DateValue = ({date}) => {
+    const DateValue = ({ date }) => {
         return (<>{formatValue(date, 'date')}</>)
     }
-    const PersonValue = ({personId}) => {
-        const {getOne} = useDeskUsers()
+    const PersonValue = ({ personId }) => {
+        const { getOne } = useDeskUsers()
         const person = getOne(personId)
         return (<>{person && person.displayName}</>)
     }
-    const SuggestedActivity = ({activity}) => {
-        const {executeActivity} = useDeskTickets()
+    const RelatedClient = ({ relatedClient, onClick }) => {
+        const { addRelatedClients, removeRelatedClients } = useDeskTickets();
+        const handleClick = (relatedClient) => { onClick && onClick(relatedClient) };
+        const handleAddRelatedClient = () => {
+            addRelatedClients('h5S5aWFyEiCk0KrJdtNV', 'ABC')
+        }
+        const handleRemoveRelatedClient = () => {
+            removeRelatedClients('h5S5aWFyEiCk0KrJdtNV', 'ABC')
+        }
+        return (
+            <>
+                <StyledButton onClick={handleAddRelatedClient}>
+                    <AddIcon />
+                </StyledButton>
+                <StyledButton onClick={handleRemoveRelatedClient}>
+                    <DotsIcon />
+                </StyledButton>
+                <lu className={"list-group"} >
+                    {relatedClient.map((client, index) =>
+                        <LinkedClient client={{ displayName: client }} urj={"jkjk"} onClick={handleClick} />
+                        //  <div>{relatedClient}</div>
+                    )}
+                </lu>
+            </>
+        )
+    }
+    const SuggestedActivity = ({ activity }) => {
+        const { executeActivity } = useDeskTickets()
         const handleClick = (e) => {
             e.preventDefault()
             executeActivity(ticket.id, activity)
@@ -43,20 +71,20 @@ const TicketSummary = ({ticket}) => {
             </div>
         )
     }
-    const SuggestedActivities = ({activities}) => (
+    const SuggestedActivities = ({ activities }) => (
         < > {activities && Object.keys(activities).map((activity, index) => (
-            <SuggestedActivity key={index} activity={activity}/>))
+            <SuggestedActivity key={index} activity={activity} />))
         }</>
     )
 
-    const Document = ({document}) => {
+    const Document = ({ document }) => {
         const handleClick = (e) => {
             e.preventDefault()
             window.open(document.url)
         }
         return (
             <a href='' onClick={handleClick}
-               className="list-group-item list-group-item-action flex-column align-items-start">
+                className="list-group-item list-group-item-action flex-column align-items-start">
                 <div className="mx-auto text-info">
                     <small>{moment(document.receivedDate).fromNow()}</small>
                 </div>
@@ -64,73 +92,74 @@ const TicketSummary = ({ticket}) => {
             </a>
         )
     }
-    const Documents = ({documents}) => (
+    const Documents = ({ documents }) => (
         <lu className={"list-group"}>
             {documents && Object.values(documents).map((document, index) => (
-                <Document key={index} document={document}/>))}
+                <Document key={index} document={document} />))}
         </lu>
     )
 
-    const Description = ({description}) => (<p>{description}</p>)
+    const Description = ({ description }) => (<p>{description}</p>)
 
     return (
-        <Card style={{width: '400px'}}>
+        <Card style={{ width: '400px' }}>
             <CardContent>
                 <Sections title={"Ticket detail"}>
                     <Section id='information' title='Information'>
                         <DataLine label={<Label>Title</Label>}>
-                            <TitleValue/>
+                            <TitleValue />
                         </DataLine>
                         <DataLine label={<Label>Received on</Label>}>
-                            <DateValue date={ticket.receivedDate}/>
+                            <DateValue date={ticket.receivedDate} />
                         </DataLine>
                         <DataLine label={<Label>Deadline</Label>}>
-                            <DateValue date={ticket.deadlineDate}/>
+                            <DateValue date={ticket.deadlineDate} />
                         </DataLine>
                         <DataLine label={<Label>Created by</Label>}>
-                            <PersonValue personId={ticket.creatorId}/>
+                            <PersonValue personId={ticket.creatorId} />
                         </DataLine>
                         <DataLine label={<Label>Person in charge</Label>}>
-                            <PersonValue personId={ticket.assignedTo}/>
+                            <PersonValue personId={ticket.assignedTo} />
                         </DataLine>
-                        <Divider/>
+                        <Divider />
                     </Section>
                     <Section id='description' title='Description'>
-                        <Description description={ticket.description}/>
+                        <Description description={ticket.description} />
                     </Section>
-                    <Divider/>
+                    <Divider />
                     <Section id='relatedClients' title='Related Client'>
                         <DataLine label={<Label>Client</Label>}>
-                            <LinkedClient client={{displayName: "John Doe"}} urj={"jkjk"}/>
+                            {/* <LinkedClient client={{displayName: ticket.relatedClients}} urj={"jkjk"} /> */}
+                            <RelatedClient relatedClient={ticket.relatedClients} onClick={onclick} />
                         </DataLine>
                     </Section>
-                    <Divider/>
+                    <Divider />
                     <Section id='relatedContracts' title='Related Contracts'>
                         <DataLine label={<Label>Contract</Label>}>
-                            <LinkedContract client={{displayName: "UI01929821"}} urj={"jkjk"}/>
+                            <LinkedContract client={{ displayName: "UI01929821" }} urj={"jkjk"} />
                         </DataLine>
                         <DataLine label={<Label>Contract</Label>}>
-                            <LinkedContract client={{displayName: "UI07292093"}} urj={"jkjk"}/>
+                            <LinkedContract client={{ displayName: "UI07292093" }} urj={"jkjk"} />
                         </DataLine>
                         <DataLine label={<Label>Contract</Label>}>
-                            <LinkedContract client={{displayName: "MP27293032"}} urj={"jkjk"}/>
+                            <LinkedContract client={{ displayName: "MP27293032" }} urj={"jkjk"} />
                         </DataLine>
                     </Section>
-                    <Divider/>
+                    <Divider />
                     <Section id='suggestedActivities' title='Suggested activities'>
-                        <SuggestedActivities activities={ticket.suggestedActivities}/>
+                        <SuggestedActivities activities={ticket.suggestedActivities} />
                     </Section>
-                    <Divider/>
+                    <Divider />
                     <Section id='notes' title='Notes'>
                         <lu className={"list-group"}>
                             <li className="list-group-item">Note 1</li>
                             <li className="list-group-item">Note 2</li>
                         </lu>
                     </Section>
-                    <Divider/>
+                    <Divider />
                     <Section id='documents' title='Documents'>
-                        <Documents documents={ticket.documents}/>
-                        <Upload ticketId={ticket.id}/>
+                        <Documents documents={ticket.documents} />
+                        <Upload ticketId={ticket.id} />
                     </Section>
                 </Sections>
             </CardContent>
