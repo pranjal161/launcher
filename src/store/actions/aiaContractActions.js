de69b0ea5b0389f4351bfae6a600076572328585
=Norbert POINTU <=npointu@dxc.com>
2021-04-02 15:25:34 +0200

    Add risks

--- src/store/actions/aiaContractActions.js
+++ src/store/actions/aiaContractActions.js
@@ -4,25 +4,78 @@ import {aia} from "../../util/functions";
 const searchContract = (contractNumber) => AppConfig.hostUrl.defaultHostUrl + 'contracts?contract:number=' + contractNumber + '&_num=1'
 
 export const get = (contractNumber) => (dispatch, getState) => {
+    const part = 'data'
     let getContractUrl = searchContract(contractNumber)
 
-    dispatch({type: 'GET_CONTRAT_PENDING', contractNumber})
+    dispatch({type: 'FETCH_CONTRAT_DATA_START', contractNumber, part})
     return aia.get(getContractUrl).then(
         (response) => {
             if (response.data._count === 1) {
-                let result = response.data['_links']['item']
-                result = !Array.isArray(result) ? [result] : result
+                let data = response.data._links.item
+                data = !Array.isArray(data) ? [data] : data
 
-                const contractUrl = result[0].href
+                const contractUrl = data[0].href
                 aia.get(contractUrl).then((response) => {
-                    dispatch({type: 'GET_CONTRAT_SUCCESS', contractNumber, contract:response.data, url:contractUrl})
+                    dispatch({
+                        type: 'FETCH_CONTRAT_DATA_SUCCESS',
+                        contractNumber,
+                        part,
+                        data: response.data,
+                        url: contractUrl
+                    })
+                    getRisks(contractNumber)(dispatch, getState)
                     return true
                 })
             } else {
                 const errorMessage = 'Contrat not found or too many : ' + response._count
-                dispatch({type: 'GET_CONTRAT_ERROR', errorMessage})
+                dispatch({type: 'FETCH_CONTRAT_DATA_ERROR', errorMessage})
                 throw new Error(errorMessage)
             }
         }
     )
 }
+
+export const getRisks0 = (contractNumber) => (dispatch, getState) => {
+    const part = 'risks'
+    let getRisksUrl = getState().aia.entities[contractNumber].risks.url
+
+    dispatch({type: 'FETCH_CONTRAT_DATA_START', contractNumber, part})
+    return aia.get(getRisksUrl).then(
+        (response) => {
+            if (response.data._count === 1) {
+                let data = response.data._links.item
+                data = !Array.isArray(data) ? [data] : data
+
+                const contractUrl = data[0].href
+                aia.get(contractUrl).then((response) => {
+                    dispatch({type: 'FETCH_CONTRAT_DATA_SUCCESS', contractNumber, part, data: response.data})
+                    return true
+                })
+            } else {
+                const errorMessage = 'Contrat not found or too many : ' + response._count
+                dispatch({type: 'FETCH_CONTRAT_DATA_ERROR', errorMessage})
+                throw new Error(errorMessage)
+            }
+        }
+    )
+}
+
+export const getRisks = (contractNumber) => (dispatch, getState) => {
+    return fetchLink(contractNumber, 'risks') (dispatch, getState)
+}
+
+
+const fetchLink = (contractNumber, part) => (dispatch, getState) => {
+    let getLinkUrl = getState().aia.entities[contractNumber][part].url
+    dispatch({type: 'FETCH_CONTRAT_DATA_START', contractNumber, part})
+    return aia.get(getLinkUrl).then(
+        (response) => {
+            let data = response.data._links.item
+            data = !Array.isArray(data) ? [data] : data
+
+            dispatch({type: 'FETCH_CONTRAT_DATA_SUCCESS', contractNumber, part, data})
+            return true
+        }
+    )
+}
+
--- src/store/reducers/aiaReducer.js
+++ src/store/reducers/aiaReducer.js
@@ -1,28 +1,74 @@
 const initialState = {
     contracts: {},
     persons: {},
-    entities : {}
+    entities: {}
 }
+
+const getAllDataNotObject = (data) => Object.fromEntries(
+    Object.entries(data).filter(
+        ([key, value]) => typeof value !== "object")
+)
+
 
 const aiaReducer = (state = initialState, action) => {
     let contractNumber = action.contractNumber
     let newState = {...state}
     switch (action.type) {
-        case 'GET_CONTRAT_PENDING':
-            const isExist = newState.entities[contractNumber]
-            if (!isExist)
-                newState.entities[contractNumber] = {type: 'contract', id : contractNumber, status : 'fetching', timestamp : Date.now()}
+        case 'FETCH_CONTRAT_DATA_START':
+            if (!newState.entities[contractNumber])
+                newState.entities[contractNumber] = {
+                    type: 'contract',
+                    id: contractNumber,
+                    data: {
+                        status: 'fetching',
+                        timestamp: Date.now()
+                    }
+                }
             return newState
-        case 'GET_CONTRAT_SUCCESS':
-            newState.contracts[contractNumber] = action.contract
-            newState.entities[contractNumber].url = action.url
-            newState.entities[contractNumber].status = 'success'
-            newState.entities[contractNumber].timestamp = Date.now()
+        case 'FETCH_CONTRAT_DATA_SUCCESS':
+            //Part of the contract to update with action.data
+            switch (action.part) {
+                case 'data':
+                    if (!newState.contracts[contractNumber])
+                        newState.contracts[contractNumber] = {}
+                    newState.contracts[contractNumber].raw = action.data
+                    newState.contracts[contractNumber].data = getAllDataNotObject(action.data)
+
+                    //Manage links on contract entity
+                    const createLinkRef = (link) => ({
+                        url: action.data._links[link].href,
+                        status: 'not_fetch',
+                        timestamp: Date.now()
+                    })
+
+                    newState.entities[contractNumber] = {
+                        ...newState.entities[contractNumber],
+                        data: {url: action.url, status: 'success', timestamp: Date.now()},
+                        risks: createLinkRef('contract:membership_list'),
+                        roles: createLinkRef('contract:role_list'),
+                        activities: createLinkRef('cscaia:activities'),
+                        outputDocuments: createLinkRef('cscaia:output_documents'),
+                    }
+                    break;
+                case 'risks':
+                    if (!newState.contracts[contractNumber].risks)
+                        newState.contracts[contractNumber].risks = {}
+                    //newState.contracts[contractNumber].risks.raw = action.data
+                    newState.contracts[contractNumber].risks.count = action.data.length
+                    newState.contracts[contractNumber].risks.data = action.data
+                    newState.entities[contractNumber].risks = {
+                        ...newState.entities[contractNumber].risks,
+                        status: 'success',
+                        timestamp: Date.now()
+                    }
+                    break;
+
+            }
+
+
             return newState
-        case 'GET_CONTRAT_ERROR':
-            newState.entities[contractNumber].status = 'error'
-            newState.entities[contractNumber].errorMessage = action.errorMessage
-            newState.entities[contractNumber].timestamp = Date.now()
+        case 'FETCH_CONTRAT_DATA_ERROR':
+            newState.entities[contractNumber].data = {url: action.url, status: 'error', timestamp: Date.now()}
             return newState
         default:
             return state
