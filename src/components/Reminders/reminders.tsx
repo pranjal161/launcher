d3fc368d76828d9ac55828d69bc4d155520cf273
pshinde5 <pshinde5@dxc.com>
2021-04-06 20:57:50 +0530

    ..

--- src/components/Reminders/reminders.tsx
+++ src/components/Reminders/reminders.tsx
@@ -1,6 +1,5 @@
 import { DxcBox } from '@dxc-technology/halstack-react';
 import React from 'react';
-import { formatValue } from 'util/functions';
 
 const Reminders = (props: { reminders: any; }) => {
 
@@ -10,12 +9,9 @@ const Reminders = (props: { reminders: any; }) => {
         <>
             {/* Still under Construction */}
             {reminders && Object.values(reminders).map((reminder: any, index) => (
-                <div key={index}>
+                <div className="row" key={index}>
                     <DxcBox margin="xxsmall" padding="xxsmall" size="fitContent">
-                        <div>Description: {reminder.description}</div>  
-                        <div>Deadline: {formatValue(reminder.deadline, 'date')}</div>
-                        <div>Status: {reminder.status}</div>
-                        <div>Time: {reminder.time}</div>
+                        <div className="col-12">Description: {reminder.description}</div> 
                     </DxcBox>
                 </div>
             ))}
