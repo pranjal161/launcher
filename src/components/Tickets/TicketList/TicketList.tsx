f3a232378f4e6f2298f50b3601613fb3d169f6c6
stiwari80 <stiwari80@dxc.com>
2021-04-07 15:06:26 +0530

    Refactoring to Typescript

--- src/components/Tickets/TicketList/TicketList.tsx
+++ src/components/Tickets/TicketList/TicketList.tsx
@@ -6,15 +6,15 @@ import React from 'react';
  * @param {props} props Information on tickets that will be used for the display
  * @returns {*} Display of all tickets in a list
  */
-function TicketList(props) {
+const TicketList = (props:any) => {
     const {tickets, onClick, ticketId} = props
 
-    const handleClick = (ticket) => onClick && onClick(ticket)
-    const selected = (id) => ticketId === id
+    const handleClick = (ticket:any) => onClick && onClick(ticket)
+    const selected = (id:any) => ticketId === id
 
     return (
         <ul className="list-group mt-2">
-            {tickets && tickets.map((ticket) => <li key={ticket.id} className={`list-group-item ${selected(ticket.id) ? 'active' : ''}`}
+            {tickets && tickets.map((ticket:any) => <li key={ticket.id} className={`list-group-item ${selected(ticket.id) ? 'active' : ''}`}
                 onClick={() => handleClick(ticket)}>
                 <h6>{ticket.title}</h6>
                 <div className="d-flex justify-content-between">
