c54a3cad6c0c7b10ba2d41fe612541d0760e6bad
=Norbert POINTU <=npointu@dxc.com>
2021-04-07 20:06:56 +0200

    pending

--- src/data/hooks/useAia.js
+++ src/data/hooks/useAia.js
@@ -1,15 +1,62 @@
-import { fetch as fetchAction} from 'store/actions/aiaActions'
+import {fetch as fetchAction} from 'store/actions/aiaActions'
 
-import {useCallback} from "react";
+import {useCallback, useEffect, useReducer, useState} from "react";
 import {useDispatch} from "react-redux";
+
+
+
 
 /**
  * get
  * @returns {*} Information for aia
  */
 export default function useAia() {
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "set":
+                return {...state, [action.id]: action.hRef}
+            case "unMount":
+                console.log('unMount reducer', state)
+                Object.entries(state).map(([id, hRef]) => dispatch({
+                    type: 'REMOVE_SUBSCRIPTION',
+                    hRef,
+                    id
+                }))
+                return {...state}
+                break;
+            default:
+                throw new Error();
+        }
+    }
+    const [subscriptions, dispatchLocal] = useReducer(reducer, {});
+
+
+    const removeAllSubscriptions = () => Object.entries(subscriptions).map(([hRef, id]) => dispatch({
+        type: 'REMOVE_SUBSCRIPTION',
+        hRef,
+        id
+    }))
     const dispatch = useDispatch();
-    const fetch = useCallback((...params) => dispatch(fetchAction(...params)), [dispatch])
+    const fetch =
+        (...params) => {
+            const hRef = params[0]
+            const id = Date.now()
+            console.log(params, {id, hRef})
+            dispatchLocal({ type: "set",  id, hRef})
+            dispatch({type: 'ADD_SUBSCRIPTION', hRef, id})
+            dispatch(fetchAction(...params))
+
+        }
+
+    useEffect(() => {
+        console.log('subscriptions', subscriptions)
+    }, [subscriptions])
+
+    useEffect(() => {
+        return () => {
+            dispatchLocal({ type: "unMount" })
+        }
+    }, [])
 
     return {fetch}
 }
--- src/store/reducers/aiaReducer.js
+++ src/store/reducers/aiaReducer.js
@@ -1,10 +1,12 @@
 const initialState = {
-    hRefs: {}
+    hRefs: {},
+    subscriptions:{}
 }
 
 const aiaReducer = (state = initialState, action) => {
     const {hRef, data, error, timestamp} = action
     const newState = {...state}
+    let arr=[]
 
     switch (action.type) {
         case 'FETCH_HREF_START':
@@ -31,6 +33,17 @@ const aiaReducer = (state = initialState, action) => {
 
         case 'UPDATE_FETCH_HREF_ERROR':
             newState.hRefs[action.hRef] = {status:'error', hRef, error, timestamp}
+            return newState
+
+        case 'ADD_SUBSCRIPTION':
+            arr = newState.subscriptions[action.hRef]?newState.subscriptions[action.hRef]:[]
+            arr.push(action.id)
+            newState.subscriptions[action.hRef] = arr
+            return newState
+
+        case 'REMOVE_SUBSCRIPTION':
+            arr = newState.subscriptions[action.hRef]?newState.subscriptions[action.hRef]:[]
+            newState.subscriptions[action.hRef] = arr.filter((id) => id != action.id)
             return newState
 
         default:
