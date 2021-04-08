1d4a79d4152e781dacae2c8bcf049061dbc03296
=Norbert POINTU <=npointu@dxc.com>
2021-04-08 07:41:25 +0200

    it works

--- src/data/hooks/useAia.js
+++ src/data/hooks/useAia.js
@@ -1,9 +1,6 @@
+import {useCallback, useEffect, useReducer} from "react";
 import {fetch as fetchAction} from 'store/actions/aiaActions'
-
-import {useCallback, useEffect, useReducer, useState} from "react";
 import {useDispatch} from "react-redux";
-
-
 
 
 /**
@@ -11,52 +8,38 @@ import {useDispatch} from "react-redux";
  * @returns {*} Information for aia
  */
 export default function useAia() {
-    const reducer = (state, action) => {
+    const reducerLocal = (state, action) => {
         switch (action.type) {
             case "set":
                 return {...state, [action.id]: action.hRef}
             case "unMount":
-                console.log('unMount reducer', state)
-                Object.entries(state).map(([id, hRef]) => dispatch({
+                Object.entries(state).forEach(([id, hRef]) => dispatch({
                     type: 'REMOVE_SUBSCRIPTION',
                     hRef,
                     id
                 }))
                 return {...state}
-                break;
             default:
                 throw new Error();
         }
     }
-    const [subscriptions, dispatchLocal] = useReducer(reducer, {});
-
-
-    const removeAllSubscriptions = () => Object.entries(subscriptions).map(([hRef, id]) => dispatch({
-        type: 'REMOVE_SUBSCRIPTION',
-        hRef,
-        id
-    }))
+    const [, dispatchLocal] = useReducer(reducerLocal, {});
     const dispatch = useDispatch();
-    const fetch =
+
+    const fetch = useCallback(
         (...params) => {
             const hRef = params[0]
             const id = Date.now()
-            console.log(params, {id, hRef})
-            dispatchLocal({ type: "set",  id, hRef})
+            //Save localy the subscription ids
+            dispatchLocal({type: "set", id, hRef})
+            //Add the subscription into the global list
             dispatch({type: 'ADD_SUBSCRIPTION', hRef, id})
+            //dispatch the fetch action
             dispatch(fetchAction(...params))
+        }, [dispatch])
 
-        }
-
-    useEffect(() => {
-        console.log('subscriptions', subscriptions)
-    }, [subscriptions])
-
-    useEffect(() => {
-        return () => {
-            dispatchLocal({ type: "unMount" })
-        }
-    }, [])
+    //On component unmount, we unsubscribe its subscription
+    useEffect(() => (dispatchLocal({type: "unMount"})), [])
 
     return {fetch}
 }
