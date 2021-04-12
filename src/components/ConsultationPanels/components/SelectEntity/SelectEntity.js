2d0383a8aa966e818f362bdfdeafb0d9a7236d50
=Norbert POINTU <=npointu@dxc.com>
2021-04-12 12:45:47 +0200

    use DxcSelect instead of DxcDropdown

--- src/components/ConsultationPanels/components/SelectEntity/SelectEntity.js
+++ src/components/ConsultationPanels/components/SelectEntity/SelectEntity.js
@@ -1,4 +1,4 @@
-import {DxcDropdown} from "@dxc-technology/halstack-react";
+import {DxcSelect} from "@dxc-technology/halstack-react";
 import PropTypes from "prop-types";
 import React from 'react';
 import SectionHeader
@@ -10,16 +10,17 @@ const SelectEntity = ({entities, value, onChange}) => {
     if (!value && entities.length) {
         //By default, we display the first item of entities
         currentValue = entities[0].id
-        onChange(currentValue)
+        if (value !== currentValue)
+            onChange(currentValue)
     } else
         currentValue = value
 
     const options = entities.map((entity) => ({value: entity.id, label: entity.display}))
     return (<>
         {options.length === 1 && <SectionHeader title={options[0].label}/>}
-        {options.length > 1 && <DxcDropdown
+        {options.length > 1 && <DxcSelect
             options={options}
-            label={<SectionHeader title={currentValue}/>}
+            value={<SectionHeader title={currentValue}/>}
             onSelectOption={onChange}
             padding={{left: 0, bottom: 0, top: 0, right: 0}}
             margin={{left: 0, bottom: 0, top: 0, right: 0}}
