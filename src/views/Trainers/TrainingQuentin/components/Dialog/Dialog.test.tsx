b2ccd0b8988d31dc23848d81206e5f1153ed3464
Quentin Karczinski <quentin.karczinski@dxc.com>
2021-04-19 16:48:13 +0200

    Fix unit-test not running

--- src/views/Trainers/TrainingQuentin/components/Dialog/Dialog.test.tsx
+++ src/views/Trainers/TrainingQuentin/components/Dialog/Dialog.test.tsx
@@ -1,7 +1,23 @@
+
+import {ShallowWrapper, shallow} from 'enzyme';
+
+import Dialog from './Dialog';
 import React from 'react';
+
+interface Test {
+    isOpen: boolean
+}
+
+const setup:ShallowWrapper<Test> = ({isOpen, }: Test) => {
+    isOpen = isOpen || false;
+
+    return shallow(
+        <Dialog />
+    )
+}
 
 describe("", () => {
     test("", () => {
-        
+
     });
 })
