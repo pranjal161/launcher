6318e220ba9343cbc324a6b35216dc2eff7da8fa
akushwaha21 <akruti.kushwaha@dxc.com>
2021-04-19 13:40:35 +0530

    Global Search - API Integration and functional

--- src/views/Trainers/TrainingAkruti/index.tsx
+++ src/views/Trainers/TrainingAkruti/index.tsx
@@ -1,6 +1,19 @@
+import GlobalSearch from './components/GlobalSearch/GlobalSearch';
 import React from "react";
+import { searchInputsArray } from './components/GlobalSearch/constant';
+import { getSearchableFields } from "util/functions";
 
-const TrainingAkruti = () => (
-    <h1>Akruti</h1>
-);
+const TrainingAkruti = () => {
+
+    const getSchema = async (schema: string, rel: string) => {
+        const schemaFields: Promise<any> = await getSearchableFields(schema, rel);
+        return schemaFields;
+    }
+
+    return (<>
+        <h1>Akruti</h1>
+        <GlobalSearch config={searchInputsArray} getAvailableFilter={getSchema} />
+    </>);
+}
+
 export default TrainingAkruti;
