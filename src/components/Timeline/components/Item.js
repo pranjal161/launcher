4dc30c06ffa4dcc4d4688c45617a53ddc35ee4a3
Dynaste <maximemary.pro@gmail.com>
2021-04-19 16:09:36 +0200

    correctif for add document action

--- src/components/Timeline/components/Item.js
+++ src/components/Timeline/components/Item.js
@@ -1,45 +1,45 @@
 import React from 'react';
 import moment from "moment";
-import {DescriptionIcon} from './../../../assets/svg';
+import { DescriptionIcon } from './../../../assets/svg';
 
 import "./Item.scss";
 
-const Item = ({ item, users, basketName}) => {
+const Item = ({ item, users, basketName }) => {
 
-    
-    if(item != undefined){
-        switch(item.action) {
+
+    if (item != undefined) {
+        switch (item.action) {
             case "assignedTo":
                 return (
                     item && item.newValue ?
-                    (<div className="timeline-item-container">
-                        <div className="item-container">
-                            <div className="draft-container">
-                                <div className="circle-timeline"></div>
-                                <div className="line-timeline"></div>
-                            </div>
-                            <div className="info-container">
-
-                                <p className="username-item">{item.metadata.updatedByDisplay}</p>
-
-                                <p className="time-item">
-                                    {moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("hours") ||
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ? 
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.updatedISODate).format('DD/MM/YYYY - HH:MM')}
-                                </p>
-
-                                <div className="text-icon-container">
-                                    <DescriptionIcon />
-                                    <p className="action-item">{item.action.replace( /([A-Z])/g, " $1" )} {users[item.newValue].displayName} in {basketName}</p>
+                        (<div className="timeline-item-container">
+                            <div className="item-container">
+                                <div className="draft-container">
+                                    <div className="circle-timeline"></div>
+                                    <div className="line-timeline"></div>
                                 </div>
-                                
+                                <div className="info-container">
+
+                                    <p className="username-item">{item.metadata.updatedByDisplay}</p>
+
+                                    <p className="time-item">
+                                        {moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("hours") ||
+                                            moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ?
+                                            moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.updatedISODate).format('DD/MM/YYYY - HH:MM')}
+                                    </p>
+
+                                    <div className="text-icon-container">
+                                        <DescriptionIcon />
+                                        <p className="action-item">Change the person in charge <br /><span>'{item.metadata.updatedByDisplay}'</span> to <span>'{users[item.newValue].displayName}'</span></p>
+                                    </div>
+
+                                </div>
                             </div>
-                        </div>
-                    </div>)
-                    :
-                    (null)
+                        </div>)
+                        :
+                        (null)
                 )
-            
+
 
             case "ticketUpdated":
                 return (
@@ -56,16 +56,20 @@ const Item = ({ item, users, basketName}) => {
 
                                 <p className="time-item">
                                     {moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("hours") ||
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ? 
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.updatedISODate).format('DD/MM/YYYY - HH:MM')}
+                                        moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ?
+                                        moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.updatedISODate).format('DD/MM/YYYY - HH:MM')}
                                 </p>
 
-                                <p className="action-item">{item.action.replace( /([A-Z])/g, " $1" )} by {item.metadata.updatedByDisplay}</p>
+
+                                <div className="text-icon-container">
+                                    <DescriptionIcon />
+                                    <p className="action-item">{item.action.replace(/([A-Z])/g, " $1")} by {item.metadata.updatedByDisplay}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 )
-            
+
 
             case "createdBy":
                 return (
@@ -82,16 +86,21 @@ const Item = ({ item, users, basketName}) => {
 
                                 <p className="time-item">
                                     {moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("hours") ||
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ? 
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.timestamp).format('DD/MM/YYYY - HH:MM')}
+                                        moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ?
+                                        moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.timestamp).format('DD/MM/YYYY - HH:MM')}
                                 </p>
 
-                                <p className="action-item">{item.action.replace( /([A-Z])/g, " $1" )} {users[item.newValue].displayName}</p>
+                                <div className="text-icon-container">
+                                    <DescriptionIcon />
+                                    <p className="action-item">{item.action.replace(/([A-Z])/g, " $1")} <span>'{users[item.newValue].displayName}'</span></p>
+                                </div>
+
+
                             </div>
                         </div>
                     </div>
                 )
-            
+
 
             case "addedDocument":
                 return (
@@ -108,14 +117,20 @@ const Item = ({ item, users, basketName}) => {
 
                                 <p className="time-item">
                                     {moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("hours") ||
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ? 
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.updatedISODate).format('DD/MM/YYYY - HH:MM')}
+                                        moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ?
+                                        moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.updatedISODate).format('DD/MM/YYYY - HH:MM')}
                                 </p>
 
-                                <p className="action-item">Added new document in {basketName}</p>
-                                <img className="picture-item" src={item.newValue.url} alt={item.newValue.name}/>
-                                <p className="picture-name">{item.newValue.name}</p>
+                                <div className="text-icon-container">
+                                    <DescriptionIcon />
+                                    <p className="action-item">Added new document in {basketName}</p>
+                                </div>
+                                <div className="img-container">
+                                    <img className="picture-item" src={item.newValue.url} alt={item.newValue.name} />
+                                    <p className="picture-name">{item.newValue.name}</p>
+                                </div>
                             </div>
+
                         </div>
                     </div>
                 )
@@ -134,18 +149,22 @@ const Item = ({ item, users, basketName}) => {
 
                                 <p className="time-item">
                                     {moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("hours") ||
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ? 
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.updatedISODate).format('DD/MM/YYYY - HH:MM')}
+                                        moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ?
+                                        moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.updatedISODate).format('DD/MM/YYYY - HH:MM')}
                                 </p>
-                                        <p className="action-item">{item.action.replace( /([A-Z])/g, " $1" )} : {item.newValue}</p>
-                                
-                                
+
+
+                                <div className="text-icon-container">
+                                    <DescriptionIcon />
+                                    <p className="action-item">{item.action.replace(/([A-Z])/g, " $1")} : {item.newValue}</p>
+                                </div>
+
                             </div>
                         </div>
                     </div>
                 )
             case "executedActivity":
-                return(
+                return (
                     item &&
                     <div className="timeline-item-container">
                         <div className="item-container">
@@ -159,25 +178,30 @@ const Item = ({ item, users, basketName}) => {
 
                                 <p className="time-item">
                                     {moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("hours") ||
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ? 
-                                    moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.updatedISODate).format('DD/MM/YYYY - HH:MM')}
+                                        moment(new Date(item.metadata.updatedISODate)).fromNow("ss").includes("minutes") ?
+                                        moment(new Date(item.metadata.updatedISODate)).fromNow("ss") : moment(item.metadata.updatedISODate).format('DD/MM/YYYY - HH:MM')}
                                 </p>
 
-                                <p className="action-item">Activity {item.newValue.replace( /([A-Z])/g, " $1" )} executed</p>
+                                <div className="text-icon-container">
+                                    <DescriptionIcon />
+                                    <p className="action-item">Activity {item.newValue.replace(/([A-Z])/g, " $1")} executed</p>
+                                </div>
+
+
                             </div>
                         </div>
                     </div>
                 )
             default:
-                return(
+                return (
                     <div>
                         <p>Please insert the required props</p>
                     </div>
                 )
         }
     }
-    else{
-        return(null)
+    else {
+        return (null)
     }
 
 
