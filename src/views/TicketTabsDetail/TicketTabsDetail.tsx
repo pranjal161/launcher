96d5cb3ea6b34301938456f44148a203e6c6ff9c
Nikolay Kolov <nkolov@dxc.com>
2021-04-23 12:10:00 +0300

    add tabbed view component

---
+++ src/views/TicketTabsDetail/TicketTabsDetail.tsx
@@ -0,0 +1,82 @@
+import {DxcTable} from "@dxc-technology/halstack-react";
+import React from 'react';
+import styled from "styled-components";
+import useDeskTickets from "../../data/hooks/useDeskTickets";
+import { useSelector } from "react-redux";
+
+const MainTabbedDetailsContainer = styled.div`
+    margin-top: 1rem;
+`;
+
+const TicketTabsDetail = (/*props: {ticketId: number}*/) => {
+    const { getOne } = useDeskTickets();
+    const selectedTicketObject:any = useSelector((state:any) => state.navBarTabs.selectedTab);
+    let isWrongTabType = false;
+    if(selectedTicketObject.type === 'ticket') {
+        isWrongTabType = true;
+    }
+
+    let isNoTabIdPresent = false;
+    if(!selectedTicketObject.id) {
+        isNoTabIdPresent = true;
+    }
+
+    let ticket = null;
+    if(!isNoTabIdPresent && !isWrongTabType) {
+        ticket = getOne(selectedTicketObject.id);
+    }
+
+    return (
+        <>
+            {
+                isWrongTabType ? 
+                    <div>
+                        This isn&apos;t a ticket type.
+                    </div> : 
+                    isNoTabIdPresent ? 
+                        <div>
+                            There isn&apos;t a ticket Id.
+                        </div> :
+                        ticket ? 
+                            <MainTabbedDetailsContainer>
+                                <DxcTable>
+                                    <tr>
+                                        <td>Title: </td>
+                                        <td>{ticket.title}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Received on: </td>
+                                        <td>{ticket.receivedDate}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Deadline: </td>
+                                        <td>{ticket.deadlineDate}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Created by: </td>
+                                        <td>{ticket.createdBy}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Person in charge: </td>
+                                        <td>{ticket.assignedTo}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Description: </td>
+                                        <td>{ticket.description}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Description: </td>
+                                        <td>{ticket.description}</td>
+                                    </tr>
+                                </DxcTable>
+                            </MainTabbedDetailsContainer> :
+                            <div>
+                                fetching ticket...
+                            </div>
+            }
+        </>
+    )
+    
+}
+
+export default TicketTabsDetail;
