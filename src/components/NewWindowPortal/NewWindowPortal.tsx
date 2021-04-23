94eb7feda0819af84311a783f63dd122b1c32f3c
Nikolay Kolov <nkolov@dxc.com>
2021-04-23 16:22:15 +0300

    check if this fixes AWS issue

--- src/components/NewWindowPortal/NewWindowPortal.tsx
+++ src/components/NewWindowPortal/NewWindowPortal.tsx
@@ -20,24 +20,44 @@ import { StyleSheetManager } from 'styled-components';
  * @returns {void} attaches the css from source to destination element
  */
 function copyStyles(sourceDoc: Document, targetDoc: Document) {
-    Array.from(sourceDoc.styleSheets).forEach((styleSheet) => {
-        if (styleSheet.cssRules) { // for <style> elements
+    let sourceStyles = sourceDoc.head.querySelectorAll('style');
+
+    let indexStyledComponents = undefined;
+    // styled-components are inserted in a style tag with data-* attributes
+    // here, we check if they exist and mark the index of the style tag,
+    // so that later, in styleSheets import, it can be skipped and 
+    // be managed with styled-components StyleSheetManager
+    for(let i = 0; i < sourceStyles.length; i++) {
+        if(sourceStyles[i].dataset &&
+           sourceStyles[i].dataset.styled == "active") {
+           indexStyledComponents = i;
+           break;
+        }
+    }
+
+    for(let i:number = 0; i < sourceDoc.styleSheets.length; i++) {
+
+        if (sourceDoc.styleSheets[i].cssRules &&
+            indexStyledComponents !== undefined &&
+            i !== indexStyledComponents) { // for <style> elements
+
             const newStyleEl = sourceDoc.createElement('style');
 
-            Array.from(styleSheet.cssRules).forEach((cssRule) => {
+            Array.from(sourceDoc.styleSheets[i].cssRules).forEach((cssRule) => {
                 // write the text of each rule into the body of the style element
                 newStyleEl.appendChild(sourceDoc.createTextNode(cssRule.cssText));
             });
 
             targetDoc.head.appendChild(newStyleEl);
-        } else if (styleSheet.href) { // for <link> elements loading CSS from a URL
+        } else if (sourceDoc.styleSheets[i].href) { // for <link> elements loading CSS from a URL
             const newLinkEl = sourceDoc.createElement('link');
 
             newLinkEl.rel = 'stylesheet';
-            newLinkEl.href = styleSheet.href;
+            newLinkEl.href = sourceDoc.styleSheets[i].href!;
             targetDoc.head.appendChild(newLinkEl);
         }
-    });
+    }
+
 }
 
 const NewWindowPortal = ( props : {
@@ -82,7 +102,7 @@ const NewWindowPortal = ( props : {
     if(!windowMaximized)
         windowNotFullScreenSpecs = `,width=${windowWidth},height=${windowHeight},left=${windowLeft},top=${windowTop}`;
     else
-    windowNotFullScreenSpecs = `,width=${window.screen.availWidth},height=${window.screen.availHeight},left=0,top=0`;
+        windowNotFullScreenSpecs = `,width=${window.screen.availWidth},height=${window.screen.availHeight},left=0,top=0`;
 
     useEffect(() => {
         externalWindow = window.open('', '', windowNotFullScreenSpecs);
