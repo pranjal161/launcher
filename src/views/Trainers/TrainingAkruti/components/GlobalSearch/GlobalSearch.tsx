c30d04c7a35e4838e113d2504657d9238098bf76
akushwaha21 <akruti.kushwaha@dxc.com>
2021-04-29 14:45:44 +0530

    DAPDIG-112: remarks on PR

--- src/views/Trainers/TrainingAkruti/components/GlobalSearch/GlobalSearch.tsx
+++ src/views/Trainers/TrainingAkruti/components/GlobalSearch/GlobalSearch.tsx
@@ -1,49 +1,45 @@
 import './GlobalSearch.scss'
 
 import { DxcDate, DxcInput, DxcSelect } from '@dxc-technology/halstack-react';
-import { aia, getValues, search } from "util/functions";
+import { getValues, search } from "util/functions";
 
 import React from 'react';
 import moment from "moment";
 import { useTranslation } from 'react-i18next';
 
-const GlobalSearch = (props: { getAvailableOptions: (value: string) => { href: string, options: Array<{ label: string, value: string }> } }) => {
+const GlobalSearch = (props: { getAvailableOptions: (value: string) => { href: string, options: Array<{ label: string, value: string }> }, getSearchOptions: (url: string) => Promise<any> }) => {
 
     const { t } = useTranslation();
-    const { getAvailableOptions } = props;
+    const { getAvailableOptions, getSearchOptions } = props;
     const entityOptions = [
         { label: t('_PERSON'), value: 'person' },
         { label: t('_CONTRACT'), value: 'contract' },
         { label: t('_TICKET'), value: 'ticket' },
     ];
-    const [entity, setSelectedEntity] = React.useState('');
     const [isFieldsVisible, setShowFields] = React.useState(false);
-    const [propertyOptions, setPropertyOptions] = React.useState([{}]);
-    const [property, setSelectedProperty] = React.useState('');
-    const [propertyTitle, setSelectedPropertyTitle] = React.useState('');
-    const [searchOptions, setSearchOptions] = React.useState([]);
-    const [searchedValue, setSearchedValue] = React.useState('');
     const [searchFieldType, setSearchFieldType] = React.useState('');
-    const [selectBoxOptions, setSelectBoxOptions] = React.useState([{}]);
     const [searchDateValue, changeDateValue] = React.useState("01-01-1995");
     const [storeData, setStoreData] = React.useState({ options: {}, href: '' });
+    const [selected, setSelected] = React.useState({ entity: '', property: '', propertyTitle: '', searched: '' });
+    const [boxOptions, setOptions] = React.useState({entity: entityOptions, property: [{}], selectOptions: [], resultOptions: []});
 
+    // Function to retrieve data from parent component - Redux store
     const getRequiredData = (selectedVal: string, data: string) => {
         const value: any = getAvailableOptions(selectedVal);
         setStoreData(value);
         return value && value[data];
     }
 
+    // Function trigerred on change selection of Entity 
     const entityChange = (newValue: string) => {
-        setSelectedEntity(newValue);
-        setSearchedValue('');
+        setSelected({...selected, entity: newValue, searched: ''});
         const searchFields: Array<{ label: string, value: string }> = getRequiredData(newValue, 'options');
-        setPropertyOptions(searchFields);
+        setOptions({...boxOptions, property: searchFields});
     }
 
+    // Function trigerred on change selection of Property 
     const propertyChange = (newValue: string) => {
-        setSelectedProperty(newValue);
-        setSearchedValue('');
+        setSelected({...selected, property: newValue, searched: ''});
         const searchFields: any = storeData['options'];
         const fieldType: { format: string, oneOf: Array<{ enum: Array<[]>, title: string }> } = searchFields[newValue];
         if (fieldType && fieldType.oneOf) {
@@ -55,36 +51,34 @@ const GlobalSearch = (props: { getAvailableOptions: (value: string) => { href: s
     }
 
     React.useEffect(() => {
-        const selectedTitle = getValues(propertyOptions, 'value', property, 'label');
-        setSelectedPropertyTitle(selectedTitle);
-    }, [property]);
+        const selectedTitle = getValues(boxOptions['property'], 'value', selected['property'], 'label');
+        setSelected({...selected, propertyTitle: selectedTitle});
+    }, [selected['property']]);
 
+    // Populate the Data in Final Select Search Box
     const populateSelectBoxOptions = (oneOfValues: Array<{ enum: Array<[]>, title: string }>) => {
-        const selectBoxValues = oneOfValues.map((array) => ({ 'label': array.title, 'value': array.enum[0] }));
-        setSelectBoxOptions(selectBoxValues);
+        const selectBoxValues: any = oneOfValues.map((array) => ({ 'label': array.title, 'value': array.enum[0] }));
+        setOptions({...boxOptions, selectOptions: selectBoxValues});
     }
 
-    const typeSearchParameter = (value: string) => {
+    // Function trigerred on typing letters to search
+    const typeSearchParameter = async (value: string) => {
         const updatedValue = searchFieldType ? value : value.toUpperCase();
-        setSearchedValue(updatedValue);
-        if (entity && property && value !== '' && value.length > 3) {
+        setSelected({...selected, searched: updatedValue});
+        if (selected['entity'] && selected['property'] && value !== '' && value.length > 3) {
             const url = storeData['href'];
-            const searchUrl = search({ 'searchUrl': url, 'name': property, 'value': updatedValue });
-            const getResp = Promise.resolve(aia.get(searchUrl));
-            getResp.then((response) => {
-                if (response && response.data && response.data['_links'] && response.data['_links']['item']) {
-                    const items = response.data['_links']['item'];
-                    const results = items && items.map((data: { title: string }) => data.title);
-                    setSearchOptions(results);
-                }
-            });
+            const searchUrl = search({ 'searchUrl': url, 'name': selected['property'], 'value': updatedValue });
+            const results = await getSearchOptions(searchUrl);
+            setOptions({...boxOptions, resultOptions: results});
         }
     }
 
+    // Trigerred on Data change
     const onDateChange = (value: any) => {
         changeDateValue(value.stringValue);
     }
 
+    // Function trigerred on blur of date field
     const onDateBlur = (value: any) => {
         const formattedDate = moment(value).format("YYYY-MM-DD");
         typeSearchParameter(formattedDate);
@@ -101,13 +95,13 @@ const GlobalSearch = (props: { getAvailableOptions: (value: string) => { href: s
                                 onChange={entityChange}
                                 label="Select Entity"
                                 margin="medium"
-                                value={entity}
+                                value={selected['entity']}
                                 padding={{ top: "xxsmall", left: "medium", right: "medium", bottom: "xxsmall" }}
                             ></DxcSelect>
                         </div>
                         <div className="col-3">
                             <DxcSelect
-                                options={propertyOptions}
+                                options={boxOptions['property']}
                                 onChange={propertyChange}
                                 label="Select Field (Property)"
                                 margin="medium"
@@ -118,23 +112,23 @@ const GlobalSearch = (props: { getAvailableOptions: (value: string) => { href: s
                     <div className="col-6">
                         <div className="w-100" onClick={() => setShowFields(true)}>
                             {!searchFieldType && <DxcInput
-                                label={`Search ${propertyTitle ? propertyTitle : ''}`}
+                                label={`Search ${selected['propertyTitle'] ? selected['propertyTitle'] : ''}`}
                                 onChange={typeSearchParameter}
-                                autocompleteOptions={searchOptions}
+                                autocompleteOptions={boxOptions['resultOptions']}
                                 margin="medium"
-                                value={searchedValue}
+                                value={selected['searched']}
                             />}
                             {searchFieldType && searchFieldType === 'oneOf' &&
-                                <DxcSelect options={selectBoxOptions}
+                                <DxcSelect options={boxOptions['selectOptions']}
                                     onChange={typeSearchParameter}
-                                    label={`Select ${propertyTitle ? propertyTitle : ''}`}
+                                    label={`Select ${selected['propertyTitle'] ? selected['propertyTitle'] : ''}`}
                                     margin="medium"
                                     padding={{ top: "xxsmall", left: "medium", right: "medium", bottom: "xxsmall" }}
                                 />
                             }
                             {searchFieldType && searchFieldType === 'date' &&
                                 <DxcDate
-                                    label={`Select ${propertyTitle ? propertyTitle : ''}`}
+                                    label={`Select ${selected['propertyTitle'] ? selected['propertyTitle'] : ''}`}
                                     placeholder
                                     value={searchDateValue}
                                     format="yyyy-dd-MM"
