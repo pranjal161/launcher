ff922a17be0ba8bf2386c8206f88b2430b9f4d3d
akushwaha21 <akruti.kushwaha@dxc.com>
2021-05-06 11:29:55 +0530

    DXC Link added

--- src/components/Tickets/TicketPreview/components/RelatedSection/RelatedSection.tsx
+++ src/components/Tickets/TicketPreview/components/RelatedSection/RelatedSection.tsx
@@ -1,20 +1,26 @@
+import { DxcLink } from '@dxc-technology/halstack-react';
 import PropTypes from 'prop-types'
 import React from 'react';
 import styled from "styled-components";
 
-const Root = styled.a`
-  display: block;
-  margin-bottom: 4px;
-  color:#0b69a3;
-  font-size:14px;
+const Root = styled.div`
+  button {
+      text-align: left;
+      margin-bottom: 7px;
+
+      & > div {
+        font-size: 14px;
+        line-height: 18px;
+      }
+  }
 `;
 
 const RelatedSection: any = (props: { value: any, onClick: any, rowTitle: string }) => {
     const handleOnClick = () => props.onClick && props.onClick(props.value)
-    return (<div className="row">
+    return (<Root className="row">
         <div className="col-5">{props.rowTitle}</div>
-        <div className="col-7 pl-0"><Root onClick={handleOnClick}>{props.value.title}</Root></div>
-    </div>)
+        <div className="col-7 pl-0"><DxcLink onClick={handleOnClick} text={props.value.title}></DxcLink></div>
+    </Root>)
 }
 
 RelatedSection.propTypes = {
