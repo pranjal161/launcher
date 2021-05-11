48507dffdb3b01472cde7809b419cf99309dd03e
stiwari80 <stiwari80@dxc.com>
2021-05-11 12:55:11 +0530

    MiddleBar

--- src/views/Trainers/TrainingJeenal/components/TicketView/components/MiddleBar/MiddleBar.tsx
+++ src/views/Trainers/TrainingJeenal/components/TicketView/components/MiddleBar/MiddleBar.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import "./MiddleBar.scss";
+
+import React, { useState } from "react";
+
+import Documents from "components/Tickets/TicketPreview/components/Documents/Documents";
+import { DxcToggleGroup } from "@dxc-technology/halstack-react";
+import Notes from "components/Timeline/Notes/Notes";
 import Timeline from "components/Timeline/Timeline";
+import Upload from "components/Tickets/TicketPreview/components/Upload/Upload";
 import WithScroll from "components/WithScroll/WithScroll";
 import useDeskBaskets from "data/hooks/useDeskBaskets";
 import useDeskTickets from "data/hooks/useDeskTickets";
@@ -13,10 +20,54 @@ const MiddleBar = (props: any) => {
     const ticketId = getOne(ticket.id);
     const basket = getOneBasket(ticket.basketId);
     const basketTitle = basket && basket.title;
-    const users = getAllUsers('data');
+    const users = getAllUsers("data");
+
+    const [value, changeValue] = useState(1);
+    const onChange = (newValue:any) => {
+        changeValue(newValue);
+    };
+
+    const options = [
+        {
+            value: 1,
+            label: "Notes",
+        },
+        {
+            value: 2,
+            label: "Documents",
+        },
+        {
+            value: 3,
+            label: "History",
+        },
+    ];
+
     return (
         <WithScroll visibleHeight="800">
-            <Timeline ticket={ticketId} users={users} basketName={basketTitle} />
+            <div className = "MiddleBarHeader">
+                <DxcToggleGroup
+                    options={options}
+                    onChange={onChange}
+                    value={value}
+                    margin="medium"
+                ></DxcToggleGroup>
+
+                {value && value === 1 && (
+                    <Notes/>
+                )}
+
+                {value && value === 2 && (
+                    <>
+                        <Documents documents={ticket.documents} />
+                        <Upload ticketId={ticket.id} />
+                    </>
+                )}
+
+                {value && value === 3 && (
+                    <Timeline ticket={ticketId} users={users} basketName={basketTitle} />
+                )}
+            
+            </div>
         </WithScroll>
     )
 
